4a625f49b6e66a930c63b39464ada8586d1e916c
Josue Torres <josuetolvera@gmail.com>
2022-08-14 21:21:59 -0500

    Add a result of each round and the winner at the end

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // I need the computer choice a random value between Rock, Paper or Scissors.
-// let playerScore = 0;
-// let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 
 
@@ -19,18 +19,22 @@ function playRound (playerSelection, computerSelection) {
   } else if (playerSelection === 'scissors'  && computerSelection === 'scissors') {
     return 'you tied!';
   } else if (playerSelection === 'rock'  && computerSelection === 'paper') {
-    // computerScore += 1;
+    computerScore += 1;
     return 'you lost! paper covers rock';
   } else if (playerSelection === 'rock'  && computerSelection === 'scissors') {
-    // playerScore += 1;
+    playerScore += 1;
     return 'you won! rock breaks scissors';
   } else if (playerSelection === 'paper'  && computerSelection === 'rock') {
+    playerScore += 1;
     return 'you won! paper covers rock';
   } else if (playerSelection === 'paper'  && computerSelection === 'scissors') {
+    computerScore += 1;
     return 'you lost! scissors cuts paper';
   } else if (playerSelection === 'scissors'  && computerSelection === 'rock') {
+    computerScore += 1;
     return 'you lost! rock breaks scissors';
   } else if (playerSelection === 'rock'  && computerSelection === 'paper') {
+    playerScore += 1;
     return 'you won! scissors cuts paper';
   }
 }
@@ -44,8 +48,17 @@ function game() {
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
   }
+  if (playerScore > computerScore) {
+    console.log(`You won the game! You get ${playerScore} points against 
+    ${computerScore} points for the computer!`)
+  } else if (playerScore < computerScore) {
+    console.log(`You lost the game! You get ${playerScore} points against 
+    ${computerScore} points for the computer!`)
+  } else {
+    console.log(`You have tied! Both get ${computerScore} points each`)
+  }
 }
 
 game()
-// console.log(computerScore);
-// console.log(playerScore);
+console.log(computerScore);
+console.log(playerScore);
